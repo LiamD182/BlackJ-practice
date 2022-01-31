4ee34ffe846eae35d493d3eb9d12cf658421dcb7
Liam Dale <16dalel@utcsheffield.org.uk>
2022-01-31 14:45:23 +0000

    Made a class for the cards

---
+++ BlackJ practice/CCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJ_practice
+{
+   class CCard
+    {
+        private int suit;
+        private int value;
+        public CCard()
+        {
+            suit = 0;
+            value = 0;
+        }
+
+        public CCard(int suit, int value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public int getSuit() { return suit; }
+        public int getValue() { return value; }
+
+        public void setSuit(int suit) { this.suit = suit; }
+        public void setValue(int value) { this.value =value; }
+
+    }
+}
--- BlackJ practice/Form1.cs
+++ BlackJ practice/Form1.cs
@@ -26,5 +26,16 @@ namespace BlackJ_practice
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
